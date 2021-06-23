12d5afd4591c5ac89bf6a75256c0b8551254e347
Alejandro-va <prof.jahz@gmail.com>
2021-06-23 17:49:13 -0300

    retomandolo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import React from 'react';
-import PantallaPrincipal from './pages/PantallaPrincipal';
-import Genero from './pages/Genero';
-import Datos from './pages/Datos';
-import Horoscopo from './pages/Horoscopo';
-import './App.css';
-import { Switch, Route, Link } from 'react-router-dom';
+import React from "react";
+import PantallaPrincipal from "./pages/PantallaPrincipal";
+import Genero from "./pages/Genero";
+import Datos from "./pages/Datos";
+import Horoscopo from "./pages/Horoscopo";
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App">
-        <Switch>
-
+      <Switch>
         <Route path="/" exact>
-      <PantallaPrincipal/>
+          <PantallaPrincipal />
         </Route>
 
-        <Route path="/genero" >
-      <Genero />
-      </Route>
-
-      <Route  exact path="/datos/:genero"  component={Datos}>
-      {/* <Datos/> */}
-      </Route>
-
-        <Route path="/horoscopo" >
-      <Horoscopo/>
+        <Route path="/genero">
+          <Genero />
         </Route>
 
+        <Route path="/datos/:genero">
+          <Datos />
+        </Route>
+
+        <Route path="/horoscopo/:form">
+          <Horoscopo />
+        </Route>
       </Switch>
     </div>
   );
--- src/pages/Datos.js
+++ src/pages/Datos.js
@@ -1,85 +1,114 @@
-import React, {useState, useEffect} from 'react';
-import './Datos.css';
-import {Link, useParams} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./Datos.css";
+import { Link, useParams, Redirect } from "react-router-dom";
 import atras from "../image/Atras.png";
 import tercera from "../image/tercera.png";
 import botonNaranja from "../image/continuar.png";
 import botonVolver from "../image/volver.png";
+import Horoscopo from "./Horoscopo";
 
 const Datos = () => {
   const [data, setData] = useState([]);
-  const [form, setForm] = useState({})
+  const [form, setForm] = useState({});
+  const [cambio, setCambio] = useState(false);
+  console.log(cambio);
+  let { genero } = useParams();
 
-let {genero} = useParams()
-
- // console.log(genero)
-
+  genero = JSON.parse(genero);
+  console.log(genero);
 
   const handleChange = (e) => {
     setForm({
-      ...form, [e.target.name]: e.target.value, genero
-    })
-  }
+      ...form,
+      [e.target.name]: e.target.value,
+      genero,
+    });
+  };
 
-let url = 'http://localhost:5500/horoscopo';
+  let url = "http://localhost:5500/horoscopo";
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    console.log(form)
-  
+    console.log(form);
+
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify( form )
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
     };
-    fetch(url, requestOptions)
-       .then(res => setData([...data, res]))
-      }
-     
-  
+    fetch(url, requestOptions).then((res) => setData([...data, res]));
 
-console.log(form)
-   return ( <div className="genero-principal">
- 
-         {/* <h2>datos{genero}</h2> */}
+    setCambio(true);
+    console.log(cambio);
+  };
+  /* to={`/datos/${JSON.stringify(genero)}` */
+  if (cambio) return <Redirect to={`/horoscopo/${JSON.stringify(form)}`} />;
 
-       
+  console.log(form);
+  return (
+    <div className="genero-principal">
+      {/* <h2>datos{genero}</h2> */}
 
       <nav className="nav-genero">
-         <div className="marcoAtras">
-         <Link  to="/">
-        <img src={atras} alt=""  className="imagenAtras"/>
-         </Link>
+        <div className="marcoAtras">
+          <Link to="/">
+            <img src={atras} alt="" className="imagenAtras" />
+          </Link>
         </div>
         <label className="tuGenero">Tus Datos</label>
       </nav>
 
       <div className="segunda">
-      <img src={tercera} alt=""  className="segundaImg"/>
-      <label className="labelSegunda">INGRESÁ TUS DATOS</label>
+        <img src={tercera} alt="" className="segundaImg" />
+        <label className="labelSegunda">INGRESÁ TUS DATOS</label>
       </div>
 
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nombre"
+          value={form.name}
+          onChange={handleChange}
+          className="input"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="EMail"
+          value={form.email}
+          onChange={handleChange}
+          className="input"
+          required
+        />
+        <input
+          type="date"
+          name="date"
+          placeholder="Fecha de Nacimiento"
+          value={form.date}
+          onChange={handleChange}
+          className="input"
+          required
+        />
 
-       <form onSubmit={handleSubmit}>
-
-            <input type="text" name="name" placeholder="Nombre" value={form.name} onChange={handleChange} className="input" required/>
-       <input type="email" name="email" placeholder="EMail" value={form.email} onChange={handleChange} className="input" required/>
-       <input type="date" name="date" placeholder="Fecha de Nacimiento" value={form.date} onChange={handleChange} className="input" required/>
-
-         <div className="btn-Datos" >
-
-         <Link className="link" to="/genero">
+        <div className="btn-Datos">
+          <Link className="link" to="/genero">
             {" "}
             <img className="azul" src={botonVolver} alt="" />{" "}
           </Link>
 
-         <input title="boton enviar" alt="boton enviar" src={botonNaranja} type="image"  className="naranjado"/>
-         </div>
-         </form>
+          <input
+            title="boton enviar"
+            alt="boton enviar"
+            src={botonNaranja}
+            type="image"
+            className="naranjado"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
-
-      </div>
-   )
-}
-
-export default Datos
+export default Datos;
